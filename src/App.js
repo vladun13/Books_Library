5f2efd0ac11dd65d407c008094bfc1f976d46da7
vladun13 <nemyrovsky1311vlad@gmail.com>
2018-09-19 13:00:37 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -3,38 +3,37 @@ import './App.css';
 import Table from './components/Table'
 import Search from './components/Search'
 
+const DEFAULT_QUERY = '';
+const PATH_BASE = 'https://www.googleapis.com/books/v1/volumes?q=';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https:reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-
-  {
-    title: 'Redux',
-    url: 'https:redux.js.org/',
-    author: 'Dan Abramov',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
 
 class App extends Component {
   constructor(props){
     super(props);
 
     this.state = {
-      list,
-      searchTerm: "",
+      result: null,
+      searchTerm: "DEFAULT_QUERY",
     };
 
+    this.setSearchTopBooks = this.setSearchTopBooks.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  setSearchTopBooks(result) {
+    this.setState({ result });
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
   }
 
   onSearchChange(event){
@@ -48,18 +47,21 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
     return (
       <div className="App">
         <Search
           value = {searchTerm}
           onChange = {this.onSearchChange}
         />
-        <Table
-          list = {list}
-          pattern = {searchTerm}
-          onDismiss = {this.onDismiss}
-        />
+        { result
+          ? <Table
+          list={result.hits}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+          />
+          : null
+          }
       </div>
     );
   }
